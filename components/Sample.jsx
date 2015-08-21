698de9eaafefb6570517e3663da0f84818764fba
Sol Chea <sol@nerdwallet.com>
2015-08-21 08:09:41 -0700

    Initial commit

---
+++ components/Sample.jsx
@@ -0,0 +1,17 @@
+'use strict'
+
+var React = require('react')
+
+var Sample = React.createClass({
+
+	displayName: 'Sample',
+
+	render: function() {
+		return (
+			<div>Hello World</div>
+        )
+	}
+
+})
+
+module.exports = Sample
---
+++ index.jsx
@@ -0,0 +1,6 @@
+'use strict'
+
+var React = require('react')
+var Sample = require('./components/Sample')
+
+React.render(<Sample />, document.getElementById('content'))
